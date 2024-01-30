84fa228e4f36ebde4ae63e19f8533e481ea022d4
gewgew4 <29262845+gewgew4@users.noreply.github.com>
2024-01-30 09:04:37 -0300

    * Added exception handler middleware * Added endpoints

---
+++ Payments.Presentacion/Controllers/AuthorizationController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Payments.Application.Dtos;
+using Payments.Application.Helpers;
+using Payments.Application.Services.Interfaces;
+
+namespace Payments.Presentacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController(IAuthorizationService authorizationService) : ControllerBase
+    {
+        private readonly IAuthorizationService _authorizationService = authorizationService;
+
+        [HttpPost]
+        public async Task<ActionResult<AuthorizationDto>> Authorize(AuthorizationRequestDto request)
+        {
+            if (!ModelState.IsValid || request == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authorizationService.Authorize(request);
+
+            if (!result.Success)
+                return WebApiResponse.GetErrorResponse(result);
+
+            return Ok(result.Data);
+        }
+
+        [HttpPatch("Confirm")]
+        public async Task<ActionResult<AuthorizationDto>> Confirm(ConfirmationRequestDto request)
+        {
+            if (!ModelState.IsValid || request == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authorizationService.Confirm(request);
+
+            if (!result.Success)
+                return WebApiResponse.GetErrorResponse(result);
+
+            return Ok(result.Data);
+        }
+
+        [HttpGet("Approved")]
+        public async Task<ActionResult<List<AuthorizationDto>>> GetApproveduthorizations()
+        {
+            var result = await _authorizationService.GetAuthorizations();
+
+            if (!result.Success)
+                return WebApiResponse.GetErrorResponse(result);
+
+            return Ok(result.Data);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AuthorizationDto>>> GetAuthorizations()
+        {
+            var result = await _authorizationService.GetAuthorizations();
+
+            if (!result.Success)
+                return WebApiResponse.GetErrorResponse(result);
+
+            return Ok(result.Data);
+        }
+
+#if DEBUG
+        // Only available for debugging purposes
+        [HttpPatch]
+        public async Task<ActionResult<List<AuthorizationDto>>> Patch()
+        {
+            var result = await _authorizationService.ReverseUnconfirmed();
+
+            if (!result.Success)
+                return WebApiResponse.GetErrorResponse(result);
+
+            return Ok(result.Data);
+        }
+#endif
+    }
+}
---
+++ Payments.Presentacion/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Payments.Presentacion.Middlewares
+{
+    public class ExceptionHandlerMiddleware(RequestDelegate next, bool showRawError, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        private readonly RequestDelegate _next = next;
+        private readonly bool _showRawError = showRawError;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger = logger;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                await HandleExceptionAsync(context, error);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            _logger.LogError(error, error.Message);
+            string _error = _showRawError ? error.Message : "";
+            string _message = "Internal Server Error";
+
+            response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var result = JsonSerializer.Serialize(new { message = _message, error = _error });
+            await response.WriteAsync(result);
+        }
+    }
+}
